8f7ab459ce866263e83a24d25b3b50695ad0ef58
Shubham Sawant <imshubham.here@gmail.com>
2023-10-21 16:47:18 +0530

    LICENSE

--- assets/vendor/isotope-layout/isotope.pkgd.js
+++ assets/vendor/isotope-layout/isotope.pkgd.js
@@ -137,11 +137,7 @@ return jQueryBridget;
 
 }));
 
-/**
- * EvEmitter v1.1.0
- * Lil' event emitter
- * MIT License
- */
+
 
 /* jshint unused: true, undef: true, strict: true */
 
@@ -250,14 +246,8 @@ return EvEmitter;
 
 }));
 
-/*!
- * getSize v2.0.3
- * measure size of elements
- * MIT license
- */
 
-/* jshint browser: true, strict: true, undef: true, unused: true */
-/* globals console: false */
+
 
 ( function( window, factory ) {
   /* jshint strict: false */ /* globals define, module */
@@ -458,11 +448,6 @@ return getSize;
 
 });
 
-/**
- * matchesSelector v2.0.2
- * matchesSelector( element, '.selector' )
- * MIT license
- */
 
 /*jshint browser: true, strict: true, undef: true, unused: true */
 
@@ -512,10 +497,6 @@ return getSize;
 
 }));
 
-/**
- * Fizzy UI utils v2.0.7
- * MIT license
- */
 
 /*jshint browser: true, undef: true, unused: true, strict: true */
 
@@ -1309,11 +1290,6 @@ return Item;
 
 }));
 
-/*!
- * Outlayer v2.1.1
- * the brains and guts of a layout library
- * MIT license
- */
 
 ( function( window, factory ) {
   'use strict';
@@ -2485,13 +2461,6 @@ return Item;
 
 }));
 
-/*!
- * Masonry v4.2.1
- * Cascading grid layout library
- * https://masonry.desandro.com
- * MIT License
- * by David DeSandro
- */
 
 ( function( window, factory ) {
   // universal module definition
